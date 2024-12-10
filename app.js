0b2fd2b63606c16f949c9babb74d70cb3d0badfc
Ewa Malusecka <maluseckae@gmail.com>
2024-12-10 20:34:13 +0000

    set up postgres with sequelize and test it

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+const sequelize = require('./config/database');
+
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Autarc Backend API');
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    return sequelize.sync();
+  })
+  .then(() => {
+    console.log('All models were synchronized successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
