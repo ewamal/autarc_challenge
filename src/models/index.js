a5b367ae30fef57e4983555e995ca09f936f3ddd
Ewa Malusecka <maluseckae@gmail.com>
2024-12-11 16:33:30 +0000

    refactor

--- src/models/index.js
+++ src/models/index.js
@@ -1,16 +1,19 @@
-const sequelize = require("../config/database");
+const sequelize = require("../config/database.js");
 const Customer = require("./Customer");
 
 const initDB = async () => {
+
   try {
     await sequelize.authenticate();
     console.log("Database connected successfully.");
 
-    await sequelize.sync({ alter: true });
+    Customer.init(sequelize);
+
+    await sequelize.sync({ alter: true }); 
     console.log("All models were synchronized successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 };
 
-module.exports = { sequelize, Customer, initDB };
+module.exports = { initDB };
