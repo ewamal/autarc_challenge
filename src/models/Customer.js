37b0f33cd310a752590758a9664d44bffda754e3
Ewa Malusecka <maluseckae@gmail.com>
2024-12-11 16:32:52 +0000

    add validation to customer model

--- src/models/Customer.js
+++ src/models/Customer.js
@@ -1,31 +1,51 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes, Model } = require('sequelize');
 
-const Customer = sequelize.define('Customer', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
-    },
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-}, {
-  tableName: 'customers',
-  timestamps: true,
-});
+class Customer extends Model {
+  /**
+   * Initialize the Customer model.
+   * @param {Sequelize} sequelize - The Sequelize instance.
+   */
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            notEmpty: { msg: 'Name cannot be empty' },
+          },
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: { msg: 'Must be a valid email address' },
+            notEmpty: { msg: 'Email cannot be empty' },
+          },
+        },
+        phone: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          validate: {
+            isNumeric: { msg: 'Phone must contain only numbers' },
+          },
+        },
+
+      },
+      {
+        sequelize,
+        modelName: 'Customer',
+        tableName: 'customers',
+        timestamps: true,
+      }
+    );
+  }
+}
 
 module.exports = Customer;
